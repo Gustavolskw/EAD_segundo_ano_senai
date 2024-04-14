ebf6252b81875e4c358cbbbb952f7e7054b11c60
unknown <gustavolschmidt13@gmail.com>
2024-04-14 12:38:25 -0300

    EAD OT-8

---
+++ OT-8-HTML/js/site.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    //Carrega cabeçalho, menu e rodapé aos respectivos locais
+    $("header").load("/components/header.html");
+    $("footer").load("/components/footer.html");
+});
